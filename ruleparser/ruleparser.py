0d49e8941f9d41014ac5434b4e992a9f0a8bfd18
harunalbayrak <harunnalbayrakk278@gmail.com>
2022-04-20 09:24:26 +0300

    Add get_rule method

--- ruleparser/ruleparser.py
+++ ruleparser/ruleparser.py
@@ -5,47 +5,32 @@ class RuleParser:
         self.filename = filename
         self.ignore_comments = ignore_comments
         self.total_rules = 0
-        self.action = "empty"
-        self.header = "empty"
         self.parse_file()
 
     def __repr__(self):
-        max = 1
+        max = 5
         string = ''
         for i in range(len(self.rules)):
             if i == max:
                 break
-            rule = self.rules[i]
-            attrs = vars(rule)
-            print(', '.join("%s: %s" % item for item in attrs.items()))
-            # string = string + f'{rule.sid} - {rule.action} - {rule.header} - {rule.msg} - {rule.content}\n'
+            string += self.get_rule(i)
         return string
 
     def parse_file(self):
         self.rules = parse_file(self.filename)
+        self.total_rules = len(self.rules)
 
-    # Inefficient ? 
-    # def read_file(self):
-    #     lines = []
-    #     with open(self.filename) as f:
-    #         lines = f.readlines()
-
-    #     for line in lines:
-    #         if(line[0] == '#' and self.ignore_comments == True):
-    #             continue
-    #         if(line[0] == '\n'):
-    #             continue
-    #         self.total_rules += 1
-    #         # print(f'line {self.total_rules}: {line}')
-
-    #         words = line.split()
-    #         for i in range(len(words)):
-    #             if i == 0:
-    #                 self.action = words[i].replace('#', '')
-    #             if i > 0 and i < 7:
-    #                 self.header += words[i] + ' '
+    def get_rule(self,i):
+        string = ''
+        rule = self.rules[i]
+        string += '-----------------------\n'
+        string += f'sid: {rule.sid}\naction: {rule.action}\nheader: {rule.header}\nmsg: {rule.msg}\n'
+        string += '>------ Options ------<\n'
+        for y in rule.options:
+            string += f'{y.name}: {y.value}\n'
+        string += '-----------------------\n'
+        return string
 
 parser = RuleParser("rules/emerging-all-snort.rules",True)
-for x in parser.rules:
-    for y in x.options:
-        print(f"{y.name} - {y.value}")
+print(parser)
+    
