f69dfc891af5e27f5040c45d790051014da02d28
harunalbayrak <harunnalbayrakk278@gmail.com>
2022-05-08 14:55:59 +0300

    add nav & sidenav files

--- gui/main.py
+++ gui/main.py
@@ -7,19 +7,19 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 
 # Static file serv
-app.mount("/static", StaticFiles(directory="src/assets"), name="static")
+app.mount("/static", StaticFiles(directory="src"), name="static")
 
 # Jinja2 template directory
 templates = Jinja2Templates(directory="src")
 
 @app.get("/",response_class=HTMLResponse)
 def index(request: Request):
-    return templates.TemplateResponse("index.html",{"request": request})
+    return templates.TemplateResponse("index.html",{"request": request, "name":"Dashboard"})
 
 @app.get("/dashboard",response_class=HTMLResponse)
 def index(request: Request):
-    return templates.TemplateResponse("index.html",{"request": request})
+    return templates.TemplateResponse("pages/dashboard.html",{"request": request,"name":"Dashboard"})
 
 @app.get("/rules",response_class=HTMLResponse)
 def index(request: Request):
-    return templates.TemplateResponse("index.html",{"request": request})
+    return templates.TemplateResponse("pages/rules.html",{"request": request,"name":"Rules"})
