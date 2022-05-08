612071ea1b1ef64370da5611534062488de1f48e
harunalbayrak <harunnalbayrakk278@gmail.com>
2022-05-09 00:55:29 +0300

    add method of dbhelper

--- database/dbhelper.py
+++ database/dbhelper.py
@@ -8,6 +8,11 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from models.rule import Rule
 from models.alert import Alert
 
+tables = {"rules":1, "alerts":2}
+
+rules = [Rule("alert",2009248,"tcp","ET SHELLCODE Lindau (linkbot) xor Decoder Shellcode"),Rule("alert",2009248,"tcp","ET SHELLCODE Lindau (linkbot) xor Decoder Shellcode"),Rule("alert",2009248,"tcp","ET SHELLCODE Lindau (linkbot) xor Decoder Shellcode"),Rule("alert",2009248,"tcp","ET SHELLCODE Lindau (linkbot) xor Decoder Shellcode")]
+alerts = [Alert("21.05.2018 / 17:56:31","High","Server-MySQL","client overflow attempt"),Alert("21.05.2018 / 17:50:22","High","Server-MySQL","Bittorrent uTP peer request"),Alert("21.05.2018 / 16:42:11","Low","Server-MySQL","OpenSSL TLS change cipher spec protocol denial of service"),Alert("21.05.2018 / 14:54:02","High","Server-MySQL","ssh CRC32 overflow filter"),Alert("21.05.2018 / 10:05:53","Medium","Server-MySQL","Sipvicious User-Agent detected"),Alert("21.05.2018 / 08:44:09","High","Server-MySQL","Win.Trojan.Rombrast Trojan outbound connection")]
+
 class DBHelper:
     def __init__(self):
         try:
@@ -25,23 +30,43 @@ class DBHelper:
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
 
+    def execute_sql(self, sql):
+        try:
+            self.cursor.execute(sql)
+            print("SQL command executed successfully.")
+            self.conn.commit()
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+
     def drop_table(self,table):
         # Doping EMPLOYEE table if already exists.
         self.cursor.execute(f"DROP TABLE IF EXISTS {table}")
 
-    def rules_table(self):
-        # Creating table as per requirement
-        sql = dbstr.create_rules_table
-        self.cursor.execute(sql)
-        print("Rules table created successfully.")
-        self.conn.commit()
+    def create_tables(self):
+        for val in tables.values:
+            sql = dbstr.get_create_query(val)
+            self.execute_sql(sql)
 
-    def alerts_table(self):
-        # Creating table as per requirement
-        sql = dbstr.create_alerts_table
-        self.cursor.execute(sql)
-        print("Rules table created successfully.")
-        self.conn.commit()
+    def create_rules_table(self):
+        sql = dbstr.get_create_query(tables["rules"])
+        self.execute_sql(sql)
+
+    def create_alerts_table(self):
+        sql = dbstr.get_create_query(tables["alerts"])
+        self.execute_sql(sql)
+
+    def insert_rules_table(self):
+        sql = dbstr.get_insert_query(tables["rules"])
+        for rule in rules:
+            self.cursor.execute(sql, rule)
+            self.conn.commit()
+
+    def insert_alerts_table(self):
+        sql = dbstr.get_insert_query(tables["alerts"])
+        for alert in alerts:
+            self.cursor.execute(sql, alert)
+            self.conn.commit()
 
     def __del__(self):
         # Closing the connection
--- database/dbstr.py
+++ database/dbstr.py
@@ -1,13 +1,40 @@
-create_rules_table = '''CREATE TABLE RULES(
-                        publisher_id SERIAL PRIMARY KEY,
-                        publisher_name VARCHAR(255) NOT NULL,
-                        publisher_estd INT,
-                        publsiher_location VARCHAR(255),
-                        publsiher_type VARCHAR(255))'''
+from venv import create
 
-create_alerts_table = '''CREATE TABLE ALERTS(
+
+create_rules_table = """CREATE TABLE RULES(
                         publisher_id SERIAL PRIMARY KEY,
                         publisher_name VARCHAR(255) NOT NULL,
                         publisher_estd INT,
                         publsiher_location VARCHAR(255),
-                        publsiher_type VARCHAR(255))'''
+                        publsiher_type VARCHAR(255))"""
+
+insert_rules_table = """INSERT INTO RULES(
+                        publisher_id, publisher_name, publisher_estd,
+                        publsiher_location, publsiher_type)
+                        VALUES (%s,%s,%s,%s,%s)"""
+
+create_alerts_table = """CREATE TABLE ALERTS(
+                        publisher_id SERIAL PRIMARY KEY,
+                        publisher_name VARCHAR(255) NOT NULL,
+                        publisher_estd INT,
+                        publsiher_location VARCHAR(255),
+                        publsiher_type VARCHAR(255))"""
+
+insert_alerts_table = """INSERT INTO ALERTS(
+                        publisher_id, publisher_name, publisher_estd,
+                        publsiher_location, publsiher_type)
+                        VALUES (%s,%s,%s,%s,%s)"""
+
+def get_create_query(i):
+    match i:
+        case 0:
+            return create_rules_table
+        case 1:
+            return create_alerts_table
+
+def get_insert_query(i):
+    match i:
+        case 0:
+            return insert_rules_table
+        case 1:
+            return insert_alerts_table
