b23e4dca4abab008956cb41e8fd05d1adb67047d
harunalbayrak <harunnalbayrakk278@gmail.com>
2022-05-08 15:36:35 +0300

    add sidebar & rule model

--- gui/main.py
+++ gui/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Body, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from rule import Rule
 
 # Initiliaze
 app = FastAPI()
@@ -12,14 +13,20 @@ app.mount("/static", StaticFiles(directory="src"), name="static")
 # Jinja2 template directory
 templates = Jinja2Templates(directory="src")
 
+dashboard_card_icons = ["error","error","graphic_eq","person"]
+dashboard_card_names = ["Today's Alerts","Total Alerts","Total Rules","Total Users"]
+dashboard_card_values = [3,392,20000,2]
+
+rules = [Rule("alert",2009248,"tcp","ET SHELLCODE Lindau (linkbot) xor Decoder Shellcode"),Rule("alert",2009248,"tcp","ET SHELLCODE Lindau (linkbot) xor Decoder Shellcode"),Rule("alert",2009248,"tcp","ET SHELLCODE Lindau (linkbot) xor Decoder Shellcode"),Rule("alert",2009248,"tcp","ET SHELLCODE Lindau (linkbot) xor Decoder Shellcode")]
+
 @app.get("/",response_class=HTMLResponse)
 def index(request: Request):
-    return templates.TemplateResponse("index.html",{"request": request, "name":"Dashboard"})
+    return templates.TemplateResponse("pages/dashboard.html",{"request": request, "name":"Dashboard"})
 
 @app.get("/dashboard",response_class=HTMLResponse)
 def index(request: Request):
-    return templates.TemplateResponse("pages/dashboard.html",{"request": request,"name":"Dashboard"})
+    return templates.TemplateResponse("pages/dashboard.html",{"request": request,"name":"Dashboard", "dashboard_card_names": dashboard_card_names, "dashboard_card_values":dashboard_card_values, "dashboard_card_icons": dashboard_card_icons})
 
 @app.get("/rules",response_class=HTMLResponse)
 def index(request: Request):
-    return templates.TemplateResponse("pages/rules.html",{"request": request,"name":"Rules"})
+    return templates.TemplateResponse("pages/rules.html",{"request": request,"name":"Rules", "rules": rules})
---
+++ gui/rule.py
@@ -0,0 +1,6 @@
+class Rule:
+    def __init__(self,action,sid,protocol,message):
+        self.action = action
+        self.sid = sid
+        self.protocol = protocol
+        self.message = message
