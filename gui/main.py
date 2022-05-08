1f01e0d899c440ee0c533dffc7eb2e95d9abd3d0
harunalbayrak <harunnalbayrakk278@gmail.com>
2022-05-08 14:28:48 +0300

    add jinja2 template

--- gui/main.py
+++ gui/main.py
@@ -1,11 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
+# Initiliaze
 app = FastAPI()
 
-@app.get("/")
-def index():
-    return {"name": "First Data"}
+# Static file serv
+app.mount("/static", StaticFiles(directory="src/assets"), name="static")
 
-@app.get("/about")
-def about():
-    return {"about": "About Data"}
+# Jinja2 template directory
+templates = Jinja2Templates(directory="src")
+
+@app.get("/",response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("index.html",{"request": request})
+
+@app.get("/dashboard",response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("index.html",{"request": request})
+
+@app.get("/rules",response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("index.html",{"request": request})
