1ccf04803cfcdea418904e78f1eb29a67bb6fefa
harunalbayrak <harunnalbayrakk278@gmail.com>
2022-05-08 10:36:38 +0300

    add packetcapture & layercapture

---
+++ packetcapture/layercapture.py
@@ -0,0 +1,62 @@
+import binascii
+
+class LayerCapture:
+    def bytes_to_hex(self,bytes):
+        return bytes.hex()
+
+    def bytes_to_str(self,bytes):
+        return str(bytes, 'utf-8')
+
+    def capture_ether(self,ether_pkt):
+        print(ether_pkt.show())
+        pass
+
+    def capture_ip(self,ip_pkt):
+        print(ip_pkt.show())
+        pass
+
+    def capture_ipv6(self,ipv6_pkt):
+        print(ipv6_pkt.show())
+        pass
+
+    def capture_udp(self,udp_pkt):
+        # PacketCapture.capture_raw(udp_pkt["Raw"])
+        # print(udp_pkt.show())
+        print(binascii.hexlify(str(udp_pkt.payload)))
+        pass
+
+    def capture_tcp(self,tcp_pkt):
+        # PacketCapture.capture_raw(tcp_pkt["Raw"])
+        # print(tcp_pkt.show())
+        # print(self.bytes_to_str(tcp_pkt.payload))
+        pass
+
+    def capture_raw(self,raw_pkt):
+        # print(raw_pkt.show())
+        try:
+            print(self.bytes_to_str(raw_pkt.load))
+        except Exception as e:
+            pass
+        pass
+
+    def capture_dns(self,dns_pkt):
+        print(dns_pkt.show())
+        pass
+
+    def capture_icmp(self,icmp_pkt):
+        print(icmp_pkt.show())
+        pass
+
+    def capture_arp(self,arp_pkt):
+        print(arp_pkt.show())
+        pass
+
+    def capture_tls(self,tls_pkt):
+        # print(tls_pkt.show())
+        try:
+            # print(tls_pkt.show())
+            print(self.bytes_to_str(tls_pkt.data))
+        except Exception as e:
+            print(e)
+        
+        pass
--- packetcapture/packetcapture.py
+++ packetcapture/packetcapture.py
@@ -1,20 +1,60 @@
 import scapy.all as scapy
+from textwrap import wrap
+from layercapture import LayerCapture
+from scapy.layers.http import *
 
-def sniffing(interface):
-    scapy.sniff(iface=interface,store=False,prn=process_packet)
+class PacketCapture:
+    def __init__(self,interface,layercapture):
+        self.interface = interface
+        self.layercapture = layercapture
 
-def process_packet(packet):
-    # print(packet.show())
-    # print(packet.get_field('dst'))
-    try:
-        # print(packet.show2())
-        # print(packet.dst)
-        # print(packet.src)
-        # print(packet.type)
-        # print(packet.nextname)
-        # print(packet.payload.layers())
-        print(packet.show())
-    except:
+    def analyse_packet(self,pkt):
+        # self.layercapture.capture_ether(pkt["Ether"])
+        # self.layercapture.capture_ip(pkt["IP"])
+        # self.layercapture.capture_ipv6(pkt["IPv6"])
+        # self.layercapture.capture_ipv6(pkt["Ether"])
+        self.layercapture.capture_udp(pkt["UDP"])
+        # self.layercapture.capture_tcp(pkt["TCP"])
+        # self.layercapture.capture_raw(pkt["Raw"])
+        # self.layercapture.capture_dns(pkt["DNS"])
+        # self.layercapture.capture_icmp(pkt["ICMP"])
+        # self.layercapture.capture_arp(pkt["ARP"])
+        # self.layercapture.capture_tls(pkt["TLS Application Data"])
+
+        # print(pkt["UDP"].load)
+        # print(pkt["ICMP"].show())
         pass
 
-sniffing("wlo1")
+    def sniffing(self):
+        scapy.load_layer("tls")
+        scapy.load_layer("http")
+        scapy.sniff(iface=self.interface,store=False,prn=self.process_packet)
+
+    def process_packet(self,pkt):
+        # capture = Capture()
+        # print(packet.show())
+        # print(packet.get_field('dst'))
+        
+        try:
+            # print(packet.show2())
+            # print(packet.dst)
+            # print(packet.src)
+            # print(packet.type)
+            # print(packet.nextname)
+            # print(packet.payload.layers())
+            # print(packet.show())
+            # print(packet["UDP"].load)
+            # bytes_to_hex(packet["UDP"].load)
+            # print(packet["TCP"].show())
+            # print(packet["Raw"].show())
+            # print(str(packet.payload) + "\n \n")
+            # self.http_header(pkt)
+            self.analyse_packet(pkt)
+            pass
+        except Exception as e:
+            # print(e)
+            pass
+
+layercapture = LayerCapture()
+packetcapture = PacketCapture("wlo1",layercapture)
+packetcapture.sniffing()
